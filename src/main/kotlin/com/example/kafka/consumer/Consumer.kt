21ff6666729ee73f3714b7507f253cbb8ce8765a
Miguel Gomes <jmbg.41@gmail.com>
2018-11-05 19:08:49 +0000

    Implement Kafka Consumer

---
+++ src/main/kotlin/com/example/kafka/consumer/Consumer.kt
@@ -0,0 +1,17 @@
+package com.example.kafka.consumer
+
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class Consumer {
+
+    private val logger = LoggerFactory.getLogger(Consumer::class.java)
+
+    @KafkaListener(topics = ["users"], groupId = "group_id")
+    fun consume(message: String) {
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+
+}
